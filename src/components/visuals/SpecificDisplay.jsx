ea6df0018a11163d0ecbfdd24c57f13952424491
mr-mph <88854453+mr-mph@users.noreply.github.com>
2023-06-22 13:10:44 -0700

    update styling

--- src/components/visuals/SpecificDisplay.jsx
+++ src/components/visuals/SpecificDisplay.jsx
@@ -1,12 +1,12 @@
 import Slider from "./Slider";
 
 export default function SpecificDisplay(props) {
-	return (
-		<div className="card row front stretch">
-			<span className="text" style={{ width: "140px" }}>
-				{props.text}
-			</span>
-			<Slider value={props.value} max={props.max} />
-		</div>
-	);
+  return (
+    <div className="card row front specific-display stretch">
+      <span className="text" style={{ width: "140px" }}>
+        {props.text}
+      </span>
+      <Slider value={props.value} max={props.max} />
+    </div>
+  );
 }
